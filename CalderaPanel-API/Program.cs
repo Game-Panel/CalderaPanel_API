efd3351fcf2bdcf0b5b9a5889194dcc20ec72824
MoskalykA <45717749+MoskalykA@users.noreply.github.com>
2021-09-18 00:20:33 +0200

    Base of the project

---
+++ CalderaPanel-API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalderaPanel_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
