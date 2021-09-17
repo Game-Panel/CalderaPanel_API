819102a4ba8fca83947bfe83df1bdf8f0fc6cbed
MoskalykA <45717749+MoskalykA@users.noreply.github.com>
2021-09-18 00:33:50 +0200

    First test of Docker SDK

--- CalderaPanel-API/Program.cs
+++ CalderaPanel-API/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
 
 namespace CalderaPanel_API
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			DockerClient client = new DockerClientConfiguration(
+				new Uri("npipe://./pipe/docker_engine"))
+				 .CreateClient();
+
+			IList<ContainerListResponse> containers = await client.Containers.ListContainersAsync(
+			new ContainersListParameters()
+			{
+				Limit = 10,
+			});
+
+			for(int i = 0; i < containers.Count; i++)
+			{
+				Console.WriteLine($"{containers[i].ImageID} - {containers[i].Image} - {containers[i].State}");
+			}
+
+			Console.ReadKey();
         }
     }
 }
